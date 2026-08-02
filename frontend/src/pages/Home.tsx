e92cfcca0ff90ff7d881bbd72777c87e70b0793f
Eduardo Luís <eduardoluisbsb2@gmail.com>
2026-08-02 01:40:20 -0300

    style: refine home page layout and content

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,12 +4,24 @@ export function Home() {
     return (
         <>
         <div className="container-home">
+
             <h1 className='header-home'>Bem-vindo à barbearia!</h1>
-            <Link to="/agendamento">Clique aqui para agendar seu horário!</Link>
-            <h2 className='subtitle-home'>Nossos serviços</h2>
-            <div className='content-home'>
-                <h3>Em manutenção</h3>
+
+            <div className='about-us'>
+                <h2>Quem somos?</h2>
+                <p>Somos uma barbearia muito prestigiada na região! Temos os melhores barbeiros e oferecemos os melhores serviços por um preço acessível, sempre pensando em você!</p>
             </div>
+            <div className='our-services'>
+                <h2>Nossos serviços</h2>
+                <div className='row-our-services'>
+                    <p>Barba</p>
+                    <p>Degradê</p>
+                    <p>Social</p>
+                </div>
+            </div>
+
+            <Link to="/agendamento">Agende já o seu horário!</Link>
+
         </div>
         </>
     )
